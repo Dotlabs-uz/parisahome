dd9b3cd0a6d389645717e936581325018c11786b
Daler <dsarifkulo021@gmail.com>
2024-09-17 17:59:20 +0500

    certificate page is created

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: '*',
+                port: '',
+                pathname: '/api/v1/uploads/**',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
