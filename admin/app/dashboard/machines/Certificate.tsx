09477670530b96d5b13e74d67c3a044ab32bffc1
Daler <dsarifkulo021@gmail.com>
2024-09-17 19:58:03 +0500

    machine manage works

--- admin/app/dashboard/machines/Certificate.tsx
+++ admin/app/dashboard/machines/Certificate.tsx
@@ -1,24 +1,54 @@
+"use client"
+import action from "@/app/actions";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 
 interface CertificateProps {
 	item: any;
+	token: { value: string };
 }
 
-const Certificate: React.FC<CertificateProps> = ({ item }) => {
-	
+const Certificate: React.FC<CertificateProps> = ({ item, token }) => {
+	async function handleRemove() {
+		const forSure = confirm("Are you sure you want to delete?");
+		if (forSure) {
+			try {
+				const res = await fetch(
+					process.env.NEXT_PUBLIC_API_URL + "/machines/" + item.id,
+					{
+						method: "delete",
+						headers: {
+							Authorization: `Bearer ${token.value}`,
+						},
+					}
+				);
+
+				if (!res.ok) {
+					throw new Error(res.statusText);
+				}
+			} catch (e: any) {
+				alert("Something went wrong!");
+				console.error(e.message);
+			}
+			action("/machines");
+		}
+	}
+
 	return (
 		<div className="w-full rounded overflow-hidden shadow-lg bg-white">
 			<Image
 				className="w-full h-48 object-cover bg-gray-200"
 				src={item.image.url}
-				alt={item.title}
-                width={200}
-                height={200}
+				alt={item.title || "parisahome"}
+				width={200}
+				height={200}
 			/>
 			<div className="px-6 py-4">
 				<div className="font-bold text-xl mb-2">{item.title}</div>
 				<p className="text-gray-700 text-base">{item.description}</p>
+				<hr className="my-4" />
+				<Button variant="destructive" onClick={handleRemove} >Delete</Button>
 			</div>
 		</div>
 	);
--- admin/app/dashboard/machines/page.tsx
+++ admin/app/dashboard/machines/page.tsx
@@ -1,5 +1,4 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
 import React from "react";
 import Certificate from "./Certificate";
 import CertificateForm from "./Create";
@@ -19,15 +18,15 @@ const page: React.FC<pageProps> = async () => {
 	return (
 		<Card className="">
 			<CardHeader>
-				<CardTitle>Manage categories</CardTitle>
+				<CardTitle>Manage machines</CardTitle>
 			</CardHeader>
 			<div className="w-full flex flex-col p-2">
 				<CertificateForm token={token} />
 				<div className="w-full">
-					<h2 className="text-2xl my-2">List of certificates</h2>
+					<h2 className="text-2xl my-2">List of machines</h2>
 					<div className="grid grid-cols-2 gap-2 w-full">
 						{data.map((item: any) => (
-							<Certificate item={item} />
+							<Certificate item={item} token={token} />
 						))}
 					</div>
 				</div>
