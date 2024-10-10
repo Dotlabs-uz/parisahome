5a699c729d4410202f80850d719df3878ff79c45
jony <sabinalatipova20@gmail.com>
2024-10-11 04:02:57 +0500

    added zoom

--- frontend/app/[lang]/[id]/components/SwiperCom.tsx
+++ frontend/app/[lang]/[id]/components/SwiperCom.tsx
@@ -1,18 +1,17 @@
-"use client"
-import React, { useState } from 'react'
-import Link from 'next/link';
-import Image from 'next/image';
+"use client";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import ZoomImage from "./ZoomImage"; // Импортируем компонент ZoomImage
 
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import { FaArrowRight } from 'react-icons/fa';
-import ZoomImageWithEffects from './ZoomImage';
-import ZoomImage from './ZoomImage';
+import { FaArrowRight } from "react-icons/fa";
+import { LuMinus, LuPlus } from "react-icons/lu";
 
 const SwiperCom = ({ data }: any) => {
-    const [swiperRef, setSwiperRef] = useState<any>(false);
+    const [swiperRef, setSwiperRef] = useState<any>(null);
+    const [scale, setScale] = useState<number>(4.5); // Стандартное значение масштаба
 
     const prevHandler = () => {
         swiperRef.slidePrev();
@@ -22,43 +21,65 @@ const SwiperCom = ({ data }: any) => {
         swiperRef.slideNext();
     };
 
+    const increaseScale = () => {
+        setScale((prevScale) => prevScale + 0.5); // Увеличиваем масштаб на 0.5
+    };
+
+    const decreaseScale = () => {
+        setScale((prevScale) => (prevScale > 1.5 ? prevScale - 0.5 : prevScale)); // Уменьшаем масштаб, но не меньше 0.5
+    };
+
     return (
-        <div className='py-28'>
-            <div className="custom-container flex justify-between items-center">
-                <div className="flex items-center gap-5">
-                    <p className='text-8xl font-bold text-white'>{data.name}</p>
-                    <div className="">
-                        <p className='text-white'>модель/серия:</p>
-                        <p className='text-6xl text-white'>{data.description}</p>
+        <div className="py-28">
+            <div className="custom-container flex justify-between sm:items-center">
+                <div className="flex max-sm:flex-col sm:items-center sm:gap-5">
+                    <p className="text-8xl max-xl:text-7xl max-md:text-6xl font-bold text-white">{data.name}</p>
+                    <div>
+                        <p className="text-white">модель/серия:</p>
+                        <p className="text-6xl max-xl:text-5xl max-md:text-3xl text-white">{data.description}</p>
                     </div>
                 </div>
                 <div className="flex gap-3">
-                    <button onClick={prevHandler} className='p-3 rounded-full border border-white'>
-                        <FaArrowRight color='#fff' className='text-[20px] rotate-180' />
+                    <button onClick={prevHandler} className="h-fit p-3 rounded-full border border-white">
+                        <FaArrowRight color="#fff" className="text-[20px] rotate-180" />
                     </button>
-                    <button onClick={nextHandler} className='p-3 rounded-full border border-white'>
-                        <FaArrowRight color='#fff' className='text-[20px]' />
+                    <button onClick={nextHandler} className="h-fit p-3 rounded-full border border-white">
+                        <FaArrowRight color="#fff" className="text-[20px]" />
                     </button>
                 </div>
             </div>
 
             <Swiper
-                className='mt-10 custom-swiper'
-                slidesPerView={2}
-                spaceBetween={20}
+                className="mt-10 custom-swiper"
+                slidesPerView={1.2}
+                spaceBetween={5}
                 centeredSlides
                 onSwiper={(swiper) => setSwiperRef(swiper)}
+                breakpoints={{
+                    960: {
+                        slidesPerView: 1.8,
+                        spaceBetween: 20
+                    }
+                }}
             >
-                {
-                    data.images.map((img: { url: string, id: number }) => (
-                        <SwiperSlide className='overflow-hidden rounded-2xl bg-[#555]' key={img.id} >
-                            <ZoomImage src={img.url} alt="Your Image" />
-                        </SwiperSlide>
-                    ))
-                }
+                {data.images.map((img: { url: string; id: number }) => (
+                    <SwiperSlide className="overflow-hidden rounded-2xl bg-[#555]" key={img.id}>
+                        <ZoomImage src={img.url} alt="Your Image" scale={scale} /> {/* Передаем масштаб */}
+                    </SwiperSlide>
+                ))}
             </Swiper>
-        </div>
-    )
-}
 
-export default SwiperCom
+            <div className="flex justify-center items-center gap-5 mt-5">
+                <button onClick={increaseScale} className="p-3 bg-white text-black rounded-full">
+                    <LuPlus color="black" className="text-[20px]" />
+                </button>
+                <p className="w-20 text-xl text-center text-white">{scale} x</p>
+                <button onClick={decreaseScale} className="p-3 bg-white text-black rounded-full">
+                    <LuMinus color="black" className="text-[20px]" />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default SwiperCom;
--- frontend/app/[lang]/[id]/components/ZoomImage.tsx
+++ frontend/app/[lang]/[id]/components/ZoomImage.tsx
@@ -1,25 +1,50 @@
+"use client"
 import Image from 'next/image';
-import React from 'react';
-import Zoom from 'react-medium-image-zoom';
-import 'react-medium-image-zoom/dist/styles.css';
+import React, { useState } from 'react';
 
 interface ZoomImageProps {
-  src: string;
-  alt: string;
+    src: string;
+    alt: string;
+    scale: number
 }
 
-const ZoomImage: React.FC<ZoomImageProps> = ({ src, alt }) => {
-  return (
-    <Zoom>
-      <Image
-        className='w-full h-full pointer-events-none select-none rounded-2xl'
-        src={src}
-        width={1000}
-        height={1000}
-        alt={alt}
-      />
-    </Zoom>
-  );
+const ZoomImage: React.FC<ZoomImageProps> = ({ src, alt, scale = 1.5 }) => {
+    const [isZoomed, setIsZoomed] = useState(false);
+    const [zoomPosition, setZoomPosition] = useState<any>({ x: 'center', y: 'center' });
+
+    const handleMouseMove = (e: any) => {
+        const { left, top, width, height } = e.target.getBoundingClientRect();
+        const x = ((e.clientX - left) / width) * 100; // Позиция по X в процентах
+        const y = ((e.clientY - top) / height) * 100; // Позиция по Y в процентах
+
+        setZoomPosition({ x, y });
+        setIsZoomed(true); // Включаем увеличение
+    };
+
+    const handleMouseLeave = () => {
+        setZoomPosition({ x: 'center', y: 'center' }); // Сбрасываем позицию
+        setIsZoomed(false); // Отключаем увеличение
+    };
+
+    return (
+        <div
+            className="image-container"
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+        >
+            <Image
+                className='w-full h-full pointer-events-none select-none rounded-2xl image zoomed'
+                style={{
+                    transformOrigin: `${zoomPosition.x}% ${zoomPosition.y}%`,
+                    transform: isZoomed ? `scale(${scale})` : 'scale(1)', // Управляем увеличением через пропс
+                }}
+                src={src}
+                width={1000}
+                height={1000}
+                alt={alt}
+            />
+        </div>
+    );
 };
 
 export default ZoomImage;
