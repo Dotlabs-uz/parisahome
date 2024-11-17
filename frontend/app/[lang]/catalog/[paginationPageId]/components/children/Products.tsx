551c05b428b23f4dcc6229c91ea64034539906c2
jony <sabinalatipova20@gmail.com>
2024-11-17 23:02:13 +0500

    is main image

--- frontend/app/[lang]/catalog/[paginationPageId]/components/children/Products.tsx
+++ frontend/app/[lang]/catalog/[paginationPageId]/components/children/Products.tsx
@@ -11,42 +11,49 @@ const Products = ({ products }: any) => {
     const productName = `${lang}Name`;
     const productDescription = `${lang}Description`;
 
+
     return (
         <div>
             <div
                 className="grid grid-cols-4 max-lg:grid-cols-3 max-sm:grid-cols-2 gap-3 gap-y-7 mb-10 max-md:mb-5"
             >
                 {
-                    products.map((i: any, idx: number) => (
-                        <Link
-                            href={`/${lang}/${i.id}`}
-                            key={idx}
-                            className="anim-element h-fit"
-                        >
-                            <div className="flex flex-col h-full w-full">
-                                <div className="w-full h-80 max-md:h-60 overflow-hidden">
-                                    <img
-                                        className="w-full h-full object-cover"
-                                        src={i.images[0].url}
-                                        // width={300}
-                                        // height={300}
-                                        alt="product"
-                                    // unoptimized
-                                    />
-                                </div>
+                    products.map((i: any, idx: number) => {
+                        const mainImage = i.images.find((img: any) => img.isMain) || i.images[0];
+                        console.log(mainImage);
 
-                                <div className="mt-1.5 px-2 text-white">
-                                    <div className="flex items-center justify-between">
-                                        <p className="font-semibold">
-                                            {i[productName]}
-                                        </p>
-                                        <p>{i.price !== 0 && `${i.price} $`}</p>
+                        return (
+                            <Link
+                                href={`/${lang}/${i.id}`}
+                                key={idx}
+                                className="anim-element h-fit"
+                            >
+                                <div className="flex flex-col h-full w-full">
+                                    <div className="w-full h-80 max-md:h-60 overflow-hidden">
+                                        <img
+                                            className="w-full h-full object-cover"
+                                            src={mainImage.url}
+                                            // width={300}
+                                            // height={300}
+                                            alt="product"
+                                            // unoptimized
+                                            loading="lazy"
+                                        />
                                     </div>
-                                    <p className="custom-truncate">{i[productDescription]}</p>
+
+                                    <div className="mt-1.5 px-2 text-white">
+                                        <div className="flex items-center justify-between">
+                                            <p className="font-semibold">
+                                                {i[productName]}
+                                            </p>
+                                            <p>{i.price !== 0 && `${i.price} $`}</p>
+                                        </div>
+                                        <p className="custom-truncate">{i[productDescription]}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
-                    ))
+                            </Link>
+                        )
+                    })
                 }
             </div>
         </div>
